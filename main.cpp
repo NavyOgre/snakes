7e69c467829b9a14264e68b5092b5cbe43f753f8
Amirreza Tavallaei <amirrezatavallaei@gmail.com>
2026-02-01 16:58:03 +0330

    Added intro message

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using std::cout;
+
+int main() {
+        cout << "Hello and welcome to my snakes and ladders game!\n";
+        cout << "================================================\n\n";
+}
