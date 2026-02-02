6fdf9d65410def279b0f98545f44dc29dcc0771a
Amirreza Tavallaei <amirrezatavallaei@gmail.com>
2026-02-02 06:28:45 +0330

    Support for multiple word names added

--- player.cpp
+++ player.cpp
@@ -4,9 +4,10 @@
 
 void get_player_info(std::vector<Player> &players, int players_cnt) {
         std::string name;
+        std::cin.ignore();
         for (int i {0}; i < players_cnt; ++i) {
                 std::cout << "Enter player " << i + 1 << " name: ";
-                std::cin >> name;
+                getline(std::cin, name);
                 players.push_back(Player {0, name, i, (i + 1) % 2, 0, 0, false, false});
         }
 }
