00d4c3f0f6d39ae122759575baf44f4f877a3858
Amirreza Tavallaei <amirrezatavallaei@gmail.com>
2026-02-06 02:45:17 +0330

    Showing New game text in game

--- main.cpp
+++ main.cpp
@@ -1,92 +1,45 @@
-#include <iostream>
 #include <cstdlib>
 #include "board.hpp"
 #include "player.hpp"
 #include "shop.hpp"
 #include "bot.hpp"
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
 
-int main() {
-        std::cout << "Hello and welcome to my snakes and ladders game!\n";
-        std::cout << "================================================\n\n\n";
-        srand(time(0));
-        std::vector<Player> players;
-        int turn {0}, total_turns {0}, load_action {};
-        bool initialized {false};
-        std::cout << "Would you like to start a new game or load the previous one?\n";
-        std::cout << "1. New game\n";
-        std::cout << "2. Load game\n";
-        std::cout << "Enter your choice: ";
-        do {
-                std::cin >> load_action;
-                if (load_action != 1 && load_action != 2) {
-                        std::cout << "Invalid choice, try again: ";
-                        continue;
+int main(int argc, char **argv) {
+        srand(time(NULL));
+        al_init();
+        al_init_font_addon();
+        al_init_ttf_addon();
+        al_install_mouse();
+        al_install_keyboard();
+        ALLEGRO_DISPLAY *display {al_create_display(640, 480)};
+        ALLEGRO_TIMER *timer {al_create_timer(1.0 / 30.0)};
+        ALLEGRO_EVENT_QUEUE *queue {al_create_event_queue()};
+        ALLEGRO_FONT *font {al_load_ttf_font("assets/font.ttf", 12, 0)};
+        al_register_event_source(queue, al_get_timer_event_source(timer));
+        al_register_event_source(queue, al_get_display_event_source(display));
+        al_register_event_source(queue, al_get_keyboard_event_source());
+        ALLEGRO_EVENT event;
+        al_start_timer(timer);
+        bool redraw {true};
+        while (true) {
+                al_wait_for_event(queue, &event);
+                if (event.type == ALLEGRO_EVENT_TIMER) {
+                        redraw = true;
+                } else if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+                        break;
                 }
-                if (load_action == 1) {
-                        int game_type {ask_game_type()};
-                        get_player_info(players, game_type);
-                        if (game_type == 1) {
-                                int difficulty {ask_difficulty()};
-                                init_bot(difficulty, players);
-                        }
-                        initialized = true;
-                } else {
-                        initialized = load_game(turn, total_turns, players);
-                        if (!initialized) {
-                                std::cout << "Couldn't load the game, choose another option: ";
-                        }
-                }
-        } while (!initialized);
-        std::string winner;
-        std::cout << std::boolalpha;
-        while (winner.empty()) {
-                int current_player {turn % 2};
-                int die_roll {};
-                int shop_selection {0};
-                if (!players.at(current_player).bot) {
-                        display_board();
-                        turn_announce(players, current_player);
-                        int action {turn_action(turn, total_turns, players)};
-                        if (action == 1) {
-                                die_roll = roll(6);
-                                std::cout << "\nRoll: " << die_roll << "\n";
-                                players.at(current_player).bonus_turn = die_roll == 6 ? true : false;
-                                ++total_turns;
-                        } else {
-                                break;
-                        }
-                        if (players.at(current_player).coin > 0) {
-                                shop_selection = get_shop_choice(players.at(current_player), die_roll);
-                        }
-                } else {
-                        die_roll = roll(6);
-                        std::cout << "\nBot rolled: " << die_roll << "\n";
-                        ++total_turns;
-                        players.at(current_player).bonus_turn = die_roll == 6 ? true : false;
-                        switch (players.at(current_player).bot) {
-                                case 1: {
-                                        shop_selection = easy_shop(players.at(current_player), die_roll);
-                                        break;
-                                }
-                                case 2: {
-                                        // normal shop
-                                        break;
-                                }
-                                case 3: {
-                                        // hard shop
-                                }
-                        }
-                }
-                shop_action(players.at(current_player), die_roll, shop_selection);
-                player_move(players, current_player, die_roll);
-                if (players.at(current_player).position == 99) {
-                        winner = players.at(current_player).name;
-                }
-                if (!players.at(current_player).bonus_turn) {
-                        ++turn;
+                if (redraw && al_is_event_queue_empty(queue)) {
+                        al_clear_to_color(al_map_rgb(0, 0, 0));
+                        al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, "New game");
+                        al_flip_display();
+                        redraw = false;
                 }
         }
-        if (!winner.empty()) {
-                std::cout << "\n\n" << winner << " won the game in " << total_turns << " turns!\n";
-        }
+        al_destroy_display(display);
+        al_destroy_timer(timer);
+        al_destroy_event_queue(queue);
+        al_destroy_font(font);
 }
